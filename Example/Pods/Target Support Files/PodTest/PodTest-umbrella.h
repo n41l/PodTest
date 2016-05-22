a24974f57b061ed2a0817bf1beb0b566b6b2fb72
HuangKun <diueym@gmail.com>
2016-05-22 19:38:32 +0800

    pod create

---
+++ Example/Pods/Target Support Files/PodTest/PodTest-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double PodTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char PodTestVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-PodTest_Example/Pods-PodTest_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PodTest_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PodTest_ExampleVersionString[];
+
